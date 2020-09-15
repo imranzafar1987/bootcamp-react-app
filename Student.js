81759304f24df9cc4d3e795da808a7471750051d
imranzafar1987 <youandme.com34@gmail.com>
2020-09-15 18:36:48 +0500

    Student.js added and used in App.js

---
+++ Student.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Student(props){
+    return(
+        <div>
+            Tell me about your progress {props.progress}
+        </div>
+    )
+}
+
+export default Student;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import Student from 'Student.js';
 
 function App() {
-  return (
+  return(
     <div>
-      <h1>Hi Man!</h1>
-      What you want to create using React ?
+      <h1>Hi man!</h1>
+      what are your feelings about learning React?
+      <Student progress='good'></Student>
     </div>
-  );
+  )
 }
 
 export default App;
