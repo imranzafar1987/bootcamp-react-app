ef10cbbca42cbd69b261c01345dfcbe92aecd982
imranzafar1987 <youandme.com34@gmail.com>
2020-12-05 18:20:56 +0500

    1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react"
-import "./App.css"
-import Student from "./Student.jsx"
-import Teacher from "./teacher.jsx"
+import React, { useState } from "react";
+import "./App.css";
+import Student from "./Student.jsx";
+import Teacher from "./teacher.jsx";
 
-  function App() {
+function App() {
   let [count, setCount] = useState(0);
   return (
     <div>
@@ -12,9 +12,17 @@ import Teacher from "./teacher.jsx"
       <Student progress="good"></Student>
       Value of count is {count}
       <br />
-      <button onClick={
-        () => { setCount(++count); }}> Count </button>
-      <Teacher teacherName="Imran Zafar"/>
+      <button
+        onClick={() => {
+          setCount(++count);
+        }}
+      >
+        Count
+      </button>
+      <Teacher teacherName="Imran Zafar" />
+      <Teacher teacherName="Kamran Zafar" />
+      <Teacher teacherName="Adnan Zafar" />
+      <Teacher teacherName="Zafar Mehmood Zafar" />
     </div>
   );
 }
--- src/teacher.jsx
+++ src/teacher.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Teacher(props) {
-  return (
-    <div>
-      I am a teacher. My name is {props.teacherName}
-    </div>
-  );
+  return <div>I am a teacher. My name is {props.teacherName}</div>;
 }
 
 export default Teacher;
